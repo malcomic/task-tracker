8886782967464a12856e45b1f69b74bd23f2bcab
Malcom Rono <118015958+malcomic@users.noreply.github.com>
2026-09-25 20:50:19 +0300

    Set base path for Vite configuration

--- task-tracker/vite.config.js
+++ task-tracker/vite.config.js
@@ -5,3 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default defineConfig({
+  base: '/your-repo-name/',
+  plugins: [react()],
+})
